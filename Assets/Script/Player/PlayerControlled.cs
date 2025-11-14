f392764d968d335b308f5082dd94a29f35495a53
yi-ye-xin-xie <yiyexinxie@gmail.com>
2025-11-14 10:56:54 +0800

    完善了角色控制脚本

--- Assets/Script/Player/PlayerControlled.cs
+++ Assets/Script/Player/PlayerControlled.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float speed;
+    public Camera cam;
+    private Vector3 movement;
     void Update()
     {
-        
+        Vector2 move = new(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+        Vector3 camForward = cam.transform.forward; camForward.y = 0f; camForward = camForward.normalized;
+        Vector3 camRight = cam.transform.right; camRight.y = 0f; camRight = camRight.normalized;
+        movement = (camForward * move.x + camRight * move.y).normalized;
+        Quaternion targetRotation = Quaternion.LookRotation(movement);
+        if (movement.magnitude > 0.1f)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 0.15f);
+        }
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
     }
 }
